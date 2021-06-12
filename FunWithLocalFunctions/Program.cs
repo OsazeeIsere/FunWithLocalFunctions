024406aee96a1b1ffd2a1e0e48f8d4b4dfde710a
Osazee Isere <oz1.concepts@gmail.com>
2021-06-12 07:58:19 +0100

    working with local functions

--- FunWithLocalFunctions/Program.cs
+++ FunWithLocalFunctions/Program.cs
@@ -10,6 +10,20 @@ namespace FunWithLocalFunctions
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("the sum is {0}", AddWrapper(5, 9));
+
         }
+
+         static int AddWrapper(int x, int y)
+        {
+            //do some validation here
+            return Add();
+          int  Add()
+          {
+                return (x + y);
+          }
+        }
+
+        
     }
 }
